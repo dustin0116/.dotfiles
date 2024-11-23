6ebd14668e048d1075a7b35a688b0ad45df0b1c7
Yen Dah Hsiang <dustin0116@gmail.com>
2024-11-23 10:28:39 -0800

    Remapped shortcuts

--- nvim/lua/config/remap.lua
+++ nvim/lua/config/remap.lua
@@ -60,14 +60,11 @@ map('n', '<Space>bw', '<Cmd>BufferOrderByWindowNumber<CR>', opts)
 
 -- Trouble.nvim
 map('n', '<leader>x', '', { desc = 'Trouble', silent = true })
-map('n', '<leader>xx', '<cmd>Trouble diagnostics toggle<cr>', { desc = 'Diagnostics (Trouble)', silent = true })
-map('n', '<leader>xX', '<cmd>Trouble diagnostics toggle filter.buf=0<cr>', { desc = 'Buffer Diagnostics (Trouble)', silent = true })
+map('n', '<leader>xx', '<cmd>Trouble diagnostics toggle filter.buf=0<cr>', { desc = 'Buffer Diagnostics (Trouble)', silent = true })
+map('n', '<leader>xX', '<cmd>Trouble diagnostics toggle<cr>', { desc = 'Diagnostics (Trouble)', silent = true })
 map('n', '<leader>xs', '<cmd>Trouble symbols toggle focus=false<cr>', { desc = 'Symbols (Trouble)', silent = true })
 map('n', '<leader>xl', '<cmd>Trouble lsp toggle focus=false win.position=right<cr>', { desc = 'TLSP Definitions / references / ... (Trouble)', silent = true })
 map('n', '<leader>xL', '<cmd>Trouble loclist toggle<cr>', { desc = 'Location List (Trouble)', silent = true })
 map('n', '<leader>xQ', '<cmd>Trouble qflist toggle<cr>', { desc = 'Quickfix List (Trouble)', silent = true })
-
-
-
 
 
